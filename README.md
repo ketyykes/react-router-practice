7ada643ba6d00d42f787ad4d259b36b8b6b1484e
ketyykes <xi4869@gmail.com>
2023-09-19 22:02:31 +0800

    firt commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Router Practice
+## React Router v6.4實作練習
+巢狀路由建置(nest route)
+使用以下API
+- createBrowserRouter
+- useRouteError
+- Outlet
+- Link
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/Root";
+import ErrorPage from "./ErrorPage";
+import Contact from "./routes/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
